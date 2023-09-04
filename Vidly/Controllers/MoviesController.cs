717b327bf6e1ac54b11ad1f991f78b057386bb52
Egisto Parronchi Neto <ticoparronchi@gmail.com>
2023-09-04 17:20:48 -0300

    exercise: add movies table

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -7,6 +7,18 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = new List<Movie>{
+               new Movie{ Name = "Shrek!" },
+               new Movie{ Name = "Hey arnold!" },
+            };
+
+            var moviesView = new MoviesViewModel{ Movies = movies };
+
+            return View(moviesView);
+        }
+        
         // GET: Movies/Random
         public ActionResult Random()
         {
